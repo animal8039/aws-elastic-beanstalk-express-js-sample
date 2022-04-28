d64922f17128a3f71cf39145f73615a976485d5f
fengzhinan <fengzhinan@meituan.com>
2022-04-28 08:16:51 +0800

    提交变更

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('这是第三次测试'));
+app.get('/', (req, res) => res.send('这是第四次测试'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
